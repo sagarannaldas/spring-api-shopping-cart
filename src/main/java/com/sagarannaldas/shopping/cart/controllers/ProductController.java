498a44e174c15972032e174905326a640c676e2f
sagarannaldas <annal7057@gmail.com>
2026-01-16 18:46:03 +0530

    1. Created post,put,delete methods for products

--- src/main/java/com/sagarannaldas/shopping/cart/controllers/ProductController.java
+++ src/main/java/com/sagarannaldas/shopping/cart/controllers/ProductController.java
@@ -3,10 +3,12 @@ package com.sagarannaldas.shopping.cart.controllers;
 import com.sagarannaldas.shopping.cart.dtos.ProductDto;
 import com.sagarannaldas.shopping.cart.entities.Product;
 import com.sagarannaldas.shopping.cart.mappers.ProductMapper;
+import com.sagarannaldas.shopping.cart.repositories.CategoryRepository;
 import com.sagarannaldas.shopping.cart.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
 
     @GetMapping
     public ResponseEntity<List<ProductDto>> getAllProducts(
@@ -43,5 +46,55 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(productMapper.toProductDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(
+            @RequestBody ProductDto productDto,
+            UriComponentsBuilder uriComponentsBuilder
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var product = productMapper.toEntity(productDto);
+        product.setCategory(category);
+        product = productRepository.save(product);
+        var productDtoNew = productMapper.toProductDto(product);
+        var uri = uriComponentsBuilder.path("/products/{id}").buildAndExpand(product.getId()).toUri();
+        return ResponseEntity.created(uri).body(productDtoNew);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductDto productDto
+    ) {
+        var category = categoryRepository.findById(productDto.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productMapper.updateProduct(productDto, product);
+        product.setCategory(category);
+        productRepository.save(product);
+        productDto.setId(product.getId());
+
+        return ResponseEntity.ok(productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable Long id
+    ) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productRepository.delete(product);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/sagarannaldas/shopping/cart/mappers/ProductMapper.java
+++ src/main/java/com/sagarannaldas/shopping/cart/mappers/ProductMapper.java
@@ -4,9 +4,15 @@ import com.sagarannaldas.shopping.cart.dtos.ProductDto;
 import com.sagarannaldas.shopping.cart.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toProductDto(Product product);
+
+    Product toEntity(ProductDto productDto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateProduct(ProductDto productDto , @MappingTarget Product product);
 }
