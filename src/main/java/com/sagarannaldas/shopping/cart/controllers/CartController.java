d91c17e271af195e0a4dd4ac80e14e55a9ca68cd
sagarannaldas <annal7057@gmail.com>
2026-01-19 15:16:36 +0530

    1. Implemented Delete method for removing cart item

--- src/main/java/com/sagarannaldas/shopping/cart/controllers/CartController.java
+++ src/main/java/com/sagarannaldas/shopping/cart/controllers/CartController.java
@@ -99,4 +99,21 @@ public class CartController {
 
         return ResponseEntity.ok(cartMapper.toDto(cartItem));
     }
+
+    @DeleteMapping("/{cartId}/items/{productId}")
+    public ResponseEntity<?> removeItem(
+            @PathVariable(name = "cartId") UUID cartId,
+            @PathVariable(name = "productId") Long productId
+    ) {
+        var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+        if (cart == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    Map.of("error", "Cart was not found.")
+            );
+
+        cart.removeItem(productId);
+        cartRepository.save(cart);
+
+        return ResponseEntity.noContent().build();
+    }
 }
