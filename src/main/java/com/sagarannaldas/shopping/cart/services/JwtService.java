78324e35ddba75271cdafd1abc17b9a5e8aa2464
sagarannaldas <annal7057@gmail.com>
2026-01-21 18:12:23 +0530

    1. Managing secret token in env file, but not working for me dont know why

--- src/main/java/com/sagarannaldas/shopping/cart/services/JwtService.java
+++ src/main/java/com/sagarannaldas/shopping/cart/services/JwtService.java
@@ -2,12 +2,17 @@ package com.sagarannaldas.shopping.cart.services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateJwtToken(String email) {
         final long tokenExpiration = 86400; // 1 day
 
@@ -15,7 +20,7 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("email".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
 }
